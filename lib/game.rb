3f6eef6fcadce2e711c880c5a38754cac9055d91
Yanis763 <yanispsn76@hotmail.com>
2020-10-15 22:32:52 +0200

    Il m'a eu

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,15 @@
 class Game
-    
+    attr_accessor :player1, :player2
     def initialize
-        play
+        @player1 = Player.new("José", "X")
+        @player2 = Player.new("Yanis", "O")
+    end
+    def lap 
+        player1lap = true
+        9.times do |lap|
+            player_game = player1
+        if player1lap == true
+            player_game
+
     end
 end
